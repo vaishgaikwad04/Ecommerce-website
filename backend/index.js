cb016f5f89f2d03fe1974761ffa2ceed3b0a5a1f
Vaishnavi Gaikwad <vaishgaikwad472@gmail.com>
2026-03-20 14:22:25 +0530

    solve cors problem

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"    ],
+      "https://ecommerce-website-app-rexe.onrender.com"    ],
     credentials: true, // allows sending cookies, auth headers, etc.
   })
 );
